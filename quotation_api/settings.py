a98ff713779911f6d2e7062db700a11ccba7a96c
NicolProper <nicole.mendoza@proper.com.pe>
2024-06-25 13:10:31 -0500

    addings tasa

--- quotation_api/settings.py
+++ quotation_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ra@6#@-i180&p6ep^6jilfvp&mo#sc7@e-l@98r!))#oe3iutg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","98jz8fku6f.execute-api.us-east-2.amazonaws.com", "ta1fvfcr52.execute-api.us-east-2.amazonaws.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","98jz8fku6f.execute-api.us-east-2.amazonaws.com", "ta1fvfcr52.execute-api.us-east-2.amazonaws.com"]
 
 
 # Application definition
