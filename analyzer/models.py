f96bbdc489fcd733de1afb4066c0d20d343060ae
NicolProper <nicole.mendoza@proper.com.pe>
2024-08-07 15:29:23 -0500

    update model Project

--- analyzer/models.py
+++ analyzer/models.py
@@ -11,7 +11,7 @@ class Analyzer(models.Model):
     tasa_credito= models.FloatField(null=False,  default=0.06)
     costo_porcentaje_capex_reparaciones=models.FloatField(null=False,  default=0) #gastos de capital
     plazo_meses = models.IntegerField(default=300)
-    descuento_porcentaje_preventa=models.FloatField(null=False,  default=0)  
+    # descuento_porcentaje_preventa=models.FloatField(null=False,  default=0)  
     costo_porcentaje_administrativos_venta=models.FloatField(null=False,  default=0) #costo de cierre
 
     def __str__(self):
